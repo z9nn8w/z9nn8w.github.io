559222b07e456131818ae89e5c7fff00fda8a719
z9nn8w <1991519466@qq.com>
2025-12-18 00:04:18 +0800

    fix LF error

--- posts/CTF/test.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-s1 = b"dGVzdA=="
-s2 = b"dGVzdA==AAABBBCCCDDD1234567890"
-
-print(base64.b64decode(s1)) # b'test'
-print(base64.b64decode(s2)) # b'test'
--- posts/CTF/wm/pdf2text/start.sh
+++
@@ -1,3 +0,0 @@
-echo $ICQ_FLAG > /flag
-unset FLAG
-python3 app.py
